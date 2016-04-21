02a64e0db3cef6aec7a7f2c7e35a461bcf149a4e
caoyuheng <caoyh.sl01@gmail.com>
2016-04-20 21:16:54 -0700

    Change IP for online

--- project2/src/main/java/project2/jdbc/JDBCProperty.java
+++ project2/src/main/java/project2/jdbc/JDBCProperty.java
@@ -2,8 +2,8 @@ package project2.jdbc;
 
 public interface JDBCProperty {
 	String DRIVER = "com.mysql.jdbc.Driver";
-	String CONNECTION_URL = "jdbc:mysql://localhost:3306/movieDB";
-	String USERNAME = "root";
+	String CONNECTION_URL = "jdbc:mysql://54.183.102.98:3306/moviedb";
+	String USERNAME = "caoyh";
 	String PASSWORD = "cyhwwq";
 	int SIZE = 20;
 }
